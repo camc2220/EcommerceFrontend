edbe2fb24d43aeb72e37086b81e79f4d7f6f4f33
camc2220 <camc2220@gmail.com>
2025-09-23 00:41:27 -0400

    1

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 3000
+    port: parseInt(process.env.PORT) || 3000
   },
   build: {
     outDir: 'dist'
